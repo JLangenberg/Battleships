f06194d774f3d82ad98df562888eeb2c5e7fc12f
JLangenberg <.>
2021-02-23 16:48:31 +0100

    Finished getIllegalFields() Method. Not tested yet.

--- SchiffeVersenken/src/ships/Ship.java
+++ SchiffeVersenken/src/ships/Ship.java
@@ -38,47 +38,122 @@ public abstract class Ship {
 	 * Calculates fields that need to be free for the ship to be placed
 	 */
 	public ArrayList<Point> getIllegalFields() {
+		// The arrayList all the bordering points will be stored in
 		ArrayList<Point> borderingPoints = new ArrayList<Point>();
 		// Get the point behind the root
-		Point point;
-		Point point2;
-		Point point3;
-		Point pointEnd;
+		Point pointBehindRoot;
+		Point pointBehindRootLeft;
+		Point pointBehindRootRight;
+		Point pointFront;
+		//TODO: Remove if not needed
+//		Point pointFrontLeft;
+//		Point pointFrontRight;
 		if (direction == 1) {
-			//TODO: Do this for the other directions too
+			// NORTH
+			
 			// The point behind the root, bordering the rear
-			point = new Point(xRoot, yRoot - 1);
-			// The point that borders the front end of the ship
-			pointEnd = new Point(xRoot, yRoot + length);
+			pointBehindRoot = new Point(xRoot, yRoot - 1);
 			// The point on the left of the point behind the root.
-			point2 = new Point(xRoot - 1, yRoot - 1);
+			pointBehindRootLeft = new Point(xRoot - 1, yRoot - 1);
 			// The point on the right of the point behind the root.
-			point3 = new Point(xRoot + 1, yRoot - 1);
-			
-			borderingPoints.add(point);
-			borderingPoints.add(point2);
-			borderingPoints.add(point3);
-			borderingPoints.add(pointEnd);
-			
-			for (int i = 0; i < length + 1; i++)	{
+			pointBehindRootRight = new Point(xRoot + 1, yRoot - 1);
+			// The point that borders the front end of the ship
+			pointFront = new Point(xRoot, yRoot + length);
+			//TODO: Remove these if they are not needed.
+//			// The point that borders the front end of the ship
+//			pointFrontLeft = new Point(xRoot - 1, yRoot + length);
+//			// The point that borders the front end of the ship
+//			pointFrontRight = new Point(xRoot + 1, yRoot + length);
+
+			for (int i = 1; i < length + 1; i++) {
 				// Get the bordering point in the direction of the ship, left side
-				Point newPoint2 = new Point(point2.getX() + i, point2.getY());
+				Point pointLeft = new Point(pointBehindRootLeft.getX(), pointBehindRootLeft.getY() + i);
 				// Get the bordering point in the direction of the ship, right side
-				Point newPoint3 = new Point(point3.getX() + i, point3.getY());
+				Point pointRight = new Point(pointBehindRootRight.getX(), pointBehindRootRight.getY() + i);
 				// Add the point to the list of bordering points
-				borderingPoints.add(newPoint2);
-				borderingPoints.add(newPoint3);
+				borderingPoints.add(pointLeft);
+				borderingPoints.add(pointRight);
 			}
 		} else if (direction == 2) {
-			point = new Point(xRoot - 1, yRoot);
+			// EAST
+
+			// Get all the points at the front and back of the ship
+
+			// The point behind the root, bordering the rear
+			pointBehindRoot = new Point(xRoot - 1, yRoot);
+			// The point on the left of the point behind the root.
+			pointBehindRootLeft = new Point(xRoot - 1, yRoot + 1);
+			// The point on the right of the point behind the root.
+			pointBehindRootRight = new Point(xRoot - 1, yRoot - 1);
+			// The point that borders the front end of the ship
+			pointFront = new Point(xRoot + length, yRoot);
+
+			for (int i = 1; i < length + 1; i++) {
+				// Get the bordering point in the direction of the ship, left side
+				Point pointLeft = new Point(pointBehindRootLeft.getX() + i, pointBehindRootLeft.getY());
+				// Get the bordering point in the direction of the ship, right side
+				Point pointRight = new Point(pointBehindRootRight.getX() + i, pointBehindRootRight.getY());
+				// Add the point to the list of bordering points
+				borderingPoints.add(pointLeft);
+				borderingPoints.add(pointRight);
+			}
 		} else if (direction == 3) {
-			point = new Point(xRoot, yRoot + 1);
+			// SOUTH
+
+			// Get all the points at the front and back of the ship
+
+			// The point behind the root, bordering the rear
+			pointBehindRoot = new Point(xRoot, yRoot + 1);
+			// The point on the left of the point behind the root.
+			pointBehindRootLeft = new Point(xRoot + 1, yRoot + 1);
+			// The point on the right of the point behind the root.
+			pointBehindRootRight = new Point(xRoot - 1, yRoot + 1);
+			// The point that borders the front end of the ship
+			pointFront = new Point(xRoot, yRoot - length);
+
+			for (int i = 1; i < length + 1; i++) {
+				// Get the bordering point in the direction of the ship, left side
+				Point pointLeft = new Point(pointBehindRootLeft.getX(), pointBehindRootLeft.getY() - i);
+				// Get the bordering point in the direction of the ship, right side
+				Point pointRight = new Point(pointBehindRootRight.getX(), pointBehindRootRight.getY() - i);
+				// Add the point to the list of bordering points
+				borderingPoints.add(pointLeft);
+				borderingPoints.add(pointRight);
+			}
 		} else if (direction == 4) {
-			point = new Point(xRoot + 1, yRoot);
+			// WEST
+
+			// Get all the points at the front and back of the ship
+
+			// The point behind the root, bordering the rear
+			pointBehindRoot = new Point(xRoot + 1, yRoot);
+			// The point on the left of the point behind the root.
+			pointBehindRootLeft = new Point(xRoot + 1, yRoot - 1);
+			// The point on the right of the point behind the root.
+			pointBehindRootRight = new Point(xRoot + 1, yRoot + 1);
+			// The point that borders the front end of the ship
+			pointFront = new Point(xRoot - length, yRoot);
+
+			for (int i = 1; i < length + 1; i++) {
+				// Get the bordering point in the direction of the ship, left side
+				Point pointLeft = new Point(pointBehindRootLeft.getX() - i, pointBehindRootLeft.getY());
+				// Get the bordering point in the direction of the ship, right side
+				Point pointRight = new Point(pointBehindRootRight.getX() - i, pointBehindRootRight.getY());
+				// Add the point to the list of bordering points
+				borderingPoints.add(pointLeft);
+				borderingPoints.add(pointRight);
+			}
 		} else {
-			point = new Point(-1, -1);
+			// If no correct direction is given, return null.
+			return null;
 		}
-		// TODO: 
+		
+		// Add all the special points to the list
+		borderingPoints.add(pointBehindRoot);
+		borderingPoints.add(pointBehindRootLeft);
+		borderingPoints.add(pointBehindRootRight);
+		borderingPoints.add(pointFront);
+		
 		return borderingPoints;
 	}
 
@@ -87,12 +162,13 @@ public abstract class Ship {
 	 * field.
 	 */
 	public ArrayList<Point> getShipFields() {
+		// The arrayList that contains all the points the ship is on
 		ArrayList<Point> shipPoints = new ArrayList<Point>();
-		// TODO:
 		// Add the root of the ship to the list of forbidden coordinates
 		shipPoints.add(new Point(xRoot, yRoot));
 		// Get all of the fields the ship is on
 		for (int i = 0; i < length; i++) {
+			// Current point to add
 			Point point;
 			if (direction == 1) {
 				point = new Point(xRoot, yRoot + i);
@@ -105,8 +181,10 @@ public abstract class Ship {
 			} else {
 				point = new Point(-1, -1);
 			}
+			// Add the calculated point to the arrayList
 			shipPoints.add(point);
 		}
+		// Return the calculated points for later usage
 		return shipPoints;
 	}
 
