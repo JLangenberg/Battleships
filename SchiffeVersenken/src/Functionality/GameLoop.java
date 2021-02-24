8c3e84fe116bad255e890a4345a256c1e7f7fd3f
JLangenberg <.>
2021-02-24 20:49:01 +0100

    Added test code.

--- SchiffeVersenken/src/Functionality/GameLoop.java
+++ SchiffeVersenken/src/Functionality/GameLoop.java
@@ -2,13 +2,16 @@ package Functionality;
 
 import gameObjects.Map;
 import gameObjects.ShipManager;
+import ships.Battleship;
 import ships.Cruiser;
 import ships.Destroyer;
+import ships.Submarine;
 
 public class GameLoop {
 	//TODO: Make a thing to create your own map and set ships
 	//TODO: Maybe make config file for stuff like ship length
 	public static void main(String[] args) {
+		
 		System.out.println("Start");
 		Map map = new Map();
 		System.out.println(map.getMapAsText());
@@ -16,11 +19,27 @@ public class GameLoop {
 		System.out.println(map.getMapAsText());
 		System.out.println("End");
 
-		//TODO: TEST THIS after finishing other thingy in ShipManager. WIth the overlapping and stuff
 		ShipManager sm = new ShipManager();
+//		private int legalAmountSubmarine = 4;
+//		private int legalAmountDestroyer = 3;
+//		private int legalAmountCruiser = 2;
+//		private int legalAmountBattleship = 1;
 		sm.placeShip(new Destroyer(0, 0, 2));
-		sm.placeShip(new Cruiser (2, 2, 3));
+		sm.placeShip(new Destroyer(0, 2, 2));
+		sm.placeShip(new Destroyer(0, 4, 2));
 		
+		sm.placeShip(new Cruiser (0, 6, 2));
+		sm.placeShip(new Cruiser (0, 8, 2));
+		
+		sm.placeShip(new Battleship(5, 9, 2));
+		
+		sm.placeShip(new Submarine(4, 0, 2));
+		sm.placeShip(new Submarine(7, 0, 2));
+		sm.placeShip(new Submarine(4, 2, 2));
+		sm.placeShip(new Submarine(4, 4, 2));
+		
+		System.out.println(sm.getShipMap());
+		//TODO: Now do the communication. Easy, right...?
 	}
 
 }
