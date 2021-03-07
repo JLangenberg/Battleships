a3a10533d1baa4ea2addfde2d7bfc0fab4282e3c
JLangenberg <.>
2021-03-07 13:48:09 +0100

    Worked on game logic. Can now fire shots. cannot receive shots yet.

--- SchiffeVersenken/src/Functionality/GameManager.java
+++ SchiffeVersenken/src/Functionality/GameManager.java
@@ -8,8 +8,12 @@ import java.net.InetAddress;
 import Utility.MessagePresets;
 import gameObjects.Map;
 import gameObjects.ShipManager;
+import gameObjects.Shot;
+import network.Listener;
+import network.Message;
 import network.Sender;
 
+//TODO: You could do an anti-cheating thing. Like when you destroyed more ships than there should be, do a notification or something.
 public abstract class GameManager {
 	// The ship manager that handles this clients ships
 	ShipManager sm;
@@ -17,41 +21,131 @@ public abstract class GameManager {
 	Map map;
 	// The address to send the messages to when playing
 	InetAddress opponentAddress;
-	
+
 	public abstract void establishConnection(Map map, ShipManager sm);
-	
-	private void fireShot()	{
-		Sender.getSender().sendMessage(opponentAddress, MessagePresets.FIRE);
-		//this.receiveShot();
-	}
-	
-	private void receiveShot()	{
-		//this.fireShot();
-	}
-	
-	//TODO: Put this into a seperate UI class. 
-	private void getShotLocation()	{
-		System.out.println("Where do you want to shoot?");
-		
-		char[] xCoordinates = {'A','B','C','D','E','F','G','H','I','J'};
-		char[] yCoordinates = {'0','1','2','3','4','5','6','7','8','9'};
 
-		String choice;
-		// Booleans that get set to true when a valid coordinate has been found.
-		boolean legalX = false;
-		boolean legalY = false;
+	private void fireShot() {
+		// Let the user input where to shoot
+		Shot shot = getShotLocation();
+		// If a valid shot was entered, tell the user input was ok and shoot.
+		System.out.println("Got it. Shooting at " + shot.getX() + shot.getY());
+		///XXX: Debug message.
+		System.out.println("Sending: " + MessagePresets.FIRE + ",[" + shot.getX() + "],[" + shot.getY() + "]");
+		// Tell the opponent where you're shooting.
+		Sender.getSender().sendMessage(opponentAddress,
+				MessagePresets.FIRE + ",[" + shot.getX() + "],[" + shot.getY() + "]");
+		// Wait for opponents response
+		getShotFeedback(shot);
+	}
 
-		// Construct the object needed for reading user input.
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		// Get a choice from the user.
-		try {
-			choice = in.readLine();
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+	private void getShotFeedback(Shot shot) {
+		// Get the response of the opponents client.
+		Message response = Listener.getListener().listenForKeywords(MessagePresets.SHOTRESPONSES, opponentAddress);
+
+		// When the shot hit, update the map, notify the user and shoot again.
+		if (response.getContent().contains(MessagePresets.HIT)) {
+			// Update the map
+			map.setFieldHit(shot.getXAsInt(), shot.getYAsInt());
+			// Tell the user he hit
+			System.out.println("HIT!");
+			// Show the updated map
+			System.out.println(map.getMapAsText());
+			// Shoot another shot
+			fireShot();
 		}
-		
+		// When the shot missed, notify the user, update the map and wait for an enemy
+		// shot.
+		if (response.getContent().contains(MessagePresets.MISS)) {
+			// Update the map
+			map.setFieldWater(shot.getXAsInt(), shot.getYAsInt());
+			// Tell the user he missed
+			System.out.println("MISS!");
+			// Show the updated map
+			System.out.println(map.getMapAsText());
+			// Wait for opponents shot
+			getEnemyShot();
+		}
+		// When the shot destroyed a ship, notify the user, update the map and shoot
+		// again.
+		if (response.getContent().contains(MessagePresets.DESTROYED)) {
+			// Update the map
+			map.setFieldHit(shot.getXAsInt(), shot.getYAsInt());
+			// Tell the user he hit and destroyed a ship.
+			System.out.println("HIT!\nShip destroyed!");
+			// Show the updated map
+			System.out.println(map.getMapAsText());
+			// Shoot another shot
+			fireShot();
+
+		}
+		// When the shot destroyed the opponents last ship, update the map and tell the
+		// user he won.
+		if (response.getContent().contains(MessagePresets.DESTROYEDLASTSHIP)) {
+			map.setFieldHit(shot.getXAsInt(), shot.getYAsInt());
+			System.out.println(map.getMapAsText());
+			System.out.println("Hit\nDestroyed last Ship! \nYou won!");
+			// TODO: Game end, won. Does something have to happen here?
+		}
+
 	}
-	
+
+	private void getEnemyShot() {
+		//TODO:XXX:DOTHISHEY Get the enemy shot and act accordingly.
+	}
+
+	// TODO: Put this into a seperate UI class.
+	private Shot getShotLocation() {
+		System.out.println("Where do you want to shoot?\nExample: 'A5'");
+
+		CharSequence[] xCoordinates = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+		CharSequence[] yCoordinates = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+		String shotX;
+		String shotY;
+		// Booleans that get set to true when a valid coordinate has been found.
+		boolean legalX;
+		boolean legalY;
+		do {
+			// String for the user input
+			String choice = null;
+			// Reset variables
+			shotX = "default";
+			shotY = "default";
+			legalX = false;
+			legalY = false;
+
+			// Construct the object needed for reading user input.
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			// Get a choice from the user.
+			try {
+				choice = in.readLine();
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// Check if input is legal
+			// Check if the message contains a valid x Coordinate.
+			for (int i = 0; i < xCoordinates.length; i++) {
+				if (choice.contains(xCoordinates[i])) {
+					shotX = xCoordinates[i].toString();
+					legalX = true;
+				}
+			}
+			// Check if the message contains a valid y Coordinate.
+			for (int i = 0; i < yCoordinates.length; i++) {
+				if (choice.contains(yCoordinates[i])) {
+					shotY = yCoordinates[i].toString();
+					legalY = true;
+				}
+			}
+			// If no legal input was found, notify the user.
+			if (!legalX || !legalY) {
+				System.out.println("Invalid input. Please try again.");
+			}
+			// Run until both a legalX and a legalY has been found
+		} while (!legalX || !legalY);
+		// Return the got shot coordinates
+		return new Shot(shotX, shotY);
+	}
 }
